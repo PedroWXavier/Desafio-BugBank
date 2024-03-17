2ac6eb4c5edaba130312365be72453bbe9c4bb21
PedroWXavier <pedro.xavier@e-unicred.com.br>
2024-03-16 23:53:50 -0300

    report

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from behave import __main__ as behave_executable
 
 if __name__ == '__main__':
-    behave_executable.main('features/transferencias/transferencia_entre_contas.feature  --no-capture --no-capture-stderr  '
-                           '--tags=-dontRun ')
-
+    behave_executable.main('features/transferencias/transferencia_entre_contas.feature  --no-capture --no-capture-stderr  --tags=-dontRun ')
