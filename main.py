0e283fc3b9a2489aebee2d82483562a49e82d22c
PedroWXavier <pedro.xavier@e-unicred.com.br>
2024-03-17 01:31:07 -0300

    gitAction config

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
+
 from behave import __main__ as behave_executable
 
+from util.driver import config
+
 if __name__ == '__main__':
+    if sys.argv[1:] and sys.argv[1] == 'git':
+        config.local_run = False
     behave_executable.main('features/transferencias/transferencia_entre_contas.feature  --no-capture --no-capture-stderr  --tags=-dontRun ')
--- util/driver/driver.py
+++ util/driver/driver.py
@@ -2,6 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.core.os_manager import ChromeType
+
+from util.driver import config
+
 driver = None
 
 
@@ -10,7 +17,27 @@ def driver_start():
 
     if driver is None:
         print("--- Iniciando chromeDriver")
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        if config.local_run:
+            driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+        else:
+            chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+            chrome_options = Options()
+            options = [
+                "--headless",
+                "--disable-gpu",
+                "--window-size=1920,1200",
+                "--ignore-certificate-errors",
+                "--disable-extensions",
+                "--no-sandbox",
+                "--disable-dev-shm-usage"
+            ]
+            for option in options:
+                chrome_options.add_argument(option)
+
+            driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
         driver.get('https://bugbank.netlify.app')
 
     return driver
