c478451f56befd51b4e5dc1251240ca92c4886ff
PedroWXavier <PedroWXavier@users.noreply.github.com>
2024-03-17 07:06:12 +0000

    deploy: 01cd042e0613576e29fdc798e5fb936b63213b6a

--- features/common/mapping/page_objects/home_objs.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class HomeObjs:
-    # ELEMENTO = (By, Locator, Descricao)
-    BOTAO_TRANSFERIR = (By.ID, 'btn-TRANSFERÊNCIA', 'Botao para ir a pagina transferir na Home')
--- features/common/mapping/page_objects/login_objs.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LoginObjs:
-    # ELEMENTO = (By, Locator, Descricao)
-    CAMPO_EMAIL = (By.NAME, 'email', 'Campo email do formulario da pagina de Login')
-    CAMPO_SENHA = (By.NAME, 'password', 'Campo senha do formulario da pagina de Login')
-    BOTAO_ACESSAR = (By.XPATH, '//form/div[3]/button[1]', 'Botao acessar da pagina de Login')
-    BOTAO_REGISTRAR = (By.XPATH, '//form/div[3]/button[2]', 'Botao registrar da pagina de Login')
